8f4679bdd1b51b6b863eeabdf1e4bdab38ae1569
Girmiti <riteshksingh76@gmail.com>
2024-06-07 19:05:54 +0530

    Bug Fix

--- app/src/main/java/com/sample/rmdbclient/presentation/di/tvShow/TvShowModule.kt
+++ app/src/main/java/com/sample/rmdbclient/presentation/di/tvShow/TvShowModule.kt
@@ -1,6 +1,7 @@
 package com.sample.rmdbclient.presentation.di.tvShow
 
 import com.sample.rmdbclient.domain.useCase.tvShowUseCase.GetTvShowUseCase
+import com.sample.rmdbclient.domain.useCase.tvShowUseCase.UpdateTvShowUseCase
 import com.sample.rmdbclient.presentation.tvShows.TvShowsViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -11,7 +12,7 @@ class TvShowModule {
     @TvShowScope
     @Provides
     fun providesTvShowViewModelFactory(getTvShowUseCase: GetTvShowUseCase,
-                                       updateTvShowUseCase: GetTvShowUseCase
+                                       updateTvShowUseCase: UpdateTvShowUseCase
     ): TvShowsViewModelFactory{
         return TvShowsViewModelFactory(getTvShowUseCase, updateTvShowUseCase)
     }
--- app/src/main/java/com/sample/rmdbclient/presentation/tvShows/TvShowsViewModelFactory.kt
+++ app/src/main/java/com/sample/rmdbclient/presentation/tvShows/TvShowsViewModelFactory.kt
@@ -3,10 +3,11 @@ package com.sample.rmdbclient.presentation.tvShows
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sample.rmdbclient.domain.useCase.tvShowUseCase.GetTvShowUseCase
+import com.sample.rmdbclient.domain.useCase.tvShowUseCase.UpdateTvShowUseCase
 
 class TvShowsViewModelFactory(
     private val getTvShowUseCase: GetTvShowUseCase,
-    private val updateTvShowUseCase: GetTvShowUseCase
+    private val updateTvShowUseCase: UpdateTvShowUseCase
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
